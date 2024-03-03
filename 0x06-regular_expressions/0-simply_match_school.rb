8e7e122756475ad953823c1025a0c735146ad9c1
Brent Hamsley <brenthamsley@gmail.com>
2024-03-02 16:29:54 -0800

    Main

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-
-# Get the argument from the command line
-input = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-  
-# Check if the input matches the pattern
-if input =~ pattern
-  puts "The input '#{input}' matches the pattern for 'School'."
-else
-  puts "The input '#{input}' does not match the pattern for 'School'."
-end
+puts ARGV[0].scan(/School/).join
