2d66491f8200f4ac32618d776b5e074c84247160
Brent Hamsley <brenthamsley@gmail.com>
2024-04-27 09:22:15 -0700

    Main

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-# Using Puppet, install flask from pip3.
+# Using Puppet, install flask version 2.1.0
 
-package { 'python3-pip':
-  ensure => installed,
+exec {'pip3 install flask':
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+exec {'python-installed':
+command => '/usr/bin/which python3'
 }
