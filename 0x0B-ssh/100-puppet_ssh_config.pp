faa18bfe001bdca4cf121576c24519272395f4e2
Brent Hamsley <brenthamsley@gmail.com>
2024-04-28 02:42:18 -0700

    Main

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Changes SSH config file
+
+  exec {'/etc/ssh/ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  }
